639df5a1c295ba827ea61467c86c2d770422486b
Tom Keays <tomkeays@gmail.com>
2013-02-03 17:38:32 -0500

    revised category.php to include cat description (date) field

--- category.php
+++ category.php
@@ -2,8 +2,19 @@
 <?php get_sidebar(); ?>
 
 			<div id="content" class="listpage">
-				<h1 class="pagetitle"><?php single_cat_title(); ?></h1>
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php $i=0; if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php 
+				    foreach( (get_the_category()) as $cat ) {
+                        $parent = get_category($cat->category_parent);
+                        if ( $parent && $parent->category_nicename == "issues" ) { // category should be child of Issues
+                            if ($i < 1) {
+                                echo "<h1 class=\"pagetitle\">".$cat->cat_name.", ".$cat->category_description;
+                                break; // only do this once. Something's wrong if it's in multiple issues
+                            }
+                        }
+                    }
+                    $i = $i + 1; 
+                    echo "</h1>"; ?>
 				<?php include (TEMPLATEPATH . '/excerpt.php'); ?>
 				<?php endwhile; endif; ?>
 			</div>
